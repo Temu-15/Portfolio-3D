28a959879038370b05699395ea81da69885081c7
Temu-15 <abuadanu89@gmail.com>
2024-09-04 15:26:15 +0300

    adjust screen

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -164,8 +164,8 @@ const ComputerCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
-          enableDamping={true}
-          dampingFactor={0.1}
+          // enableDamping={true}
+          // dampingFactor={0.1}
           enablePan={true}
           enableRotate={true}
         />
